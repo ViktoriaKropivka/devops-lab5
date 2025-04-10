d9f9fd1beef6c806845f4c8d6b2e5fcb75a26044
ViktoriaKropivka <vkkropivka05@gmail.com>
2025-04-10 23:02:06 +0300

    Fix Docker file

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,5 @@
 FROM python:3.11
-
-COPY requirements.txt .
-RUN pip install -r requirements.txt
-
 WORKDIR /app
-COPY src .
-
-ENTRYPOINT [ "python", "main.py" ]
+COPY . .
+RUN pip install fastapi uvicorn pydantic pydantic-settings httpx pytest
+CMD ["uvicorn", "src.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- tests/test_user.py
+++ tests/test_user.py
@@ -26,16 +26,45 @@ def test_get_existed_user():
 
 def test_get_unexisted_user():
     '''Получение несуществующего пользователя'''
-    pass
+    response = client.get("/api/v1/user", params={'email': "nonexistent@mail.com"})
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == {"detail": "User not found"}
 
 def test_create_user_with_valid_email():
     '''Создание пользователя с уникальной почтой'''
-    pass
+    new_user = {
+        "name": "New User",
+        "email": "new.user@mail.com"
+    }
+    response = client.post("/api/v1/user", json=new_user)
+    assert response.status_code == status.HTTP_201_CREATED
+    assert isinstance(response.json(), int)
+
+    check_response = client.get("/api/v1/user", params={'email': new_user['email']})
+    assert check_response.status_code == 200
+    assert check_response.json()['email'] == new_user['email']
+
 
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
+    existing_user = {
+        "name": "Duplicate User",
+        "email": users[0]['email']
+    }
+    response = client.post("/api/v1/user", json=existing_user)
+    assert response.status_code == status.HTTP_409_CONFLICT
+    assert response.json() == {"detail": "User with this email already exists"}
 
 def test_delete_user():
     '''Удаление пользователя'''
-    pass
+    test_user = {
+        "name": "User to delete",
+        "email": "to.delete@mail.com"
+    }
+    user_id = client.post("/api/v1/user", json=test_user).json()
+    
+    response = client.delete("/api/v1/user", params={'email': test_user['email']})
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    
+    check_response = client.get("/api/v1/user", params={'email': test_user['email']})
+    assert check_response.status_code == status.HTTP_404_NOT_FOUND
